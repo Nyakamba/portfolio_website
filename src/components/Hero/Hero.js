b05248f4361d8567defbb089548b3d2bd6133d07
Nyakamba <omwegaenock@gmail.com>
2023-05-05 23:23:53 +0300

    Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm a Front-End Software Developer by passion and an expert by choice
+        living now in Nairobi.
+        <br />I use react with with Tailwind CSS framework to develop user
+        responsive web applications.
+      </SectionText>
+      <Button onClick={() => (winndow.location = "https:google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </section>
 );
 
 export default Hero;
