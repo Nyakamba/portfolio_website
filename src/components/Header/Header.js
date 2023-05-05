95d568728440ba1c6d8ba3172205c27acbf9536a
Nyakamba <omwegaenock@gmail.com>
2023-05-05 22:24:42 +0300

    Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 import { DiCssdeck } from "react-icons/di";
 
 import {
@@ -17,10 +22,41 @@ const Header = () => (
     <Div1>
       <Link legacyBehavior href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size={"3rem"} /> <span>Portifolio</span>
+          <DiCssdeck size="3rem" /> <span>Portifolio</span>
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://twitter.com">
+        <AiFillTwitterCircle size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
