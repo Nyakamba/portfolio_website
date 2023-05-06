896db9c725c12083c934a3184259f8a391548641
Nyakamba <omwegaenock@gmail.com>
2023-05-06 19:59:24 +0300

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,32 +20,34 @@ import {
 } from "../../styles/GlobalComponents";
 import { projects } from "../../constants/constants";
 
+const projectsDemo = [
+  {
+    title: "Project 1",
+    description: "This is a really long decsription about this project",
+  },
+
+  {
+    title: "Project 2",
+    description: "This is a really long decsription about this project",
+  },
+
+  {
+    title: "Project 3",
+    description: "This is a really long decsription about this project",
+  },
+
+  {
+    title: "Project 4",
+    description: "This is a really long decsription about this project",
+  },
+];
+
 const Projects = () => (
   <section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {[
-        {
-          title: "Project 1",
-          description: "This is a really long decsription about this project",
-        },
-
-        {
-          title: "Project 2",
-          description: "This is a really long decsription about this project",
-        },
-
-        {
-          title: "Project 3",
-          description: "This is a really long decsription about this project",
-        },
-
-        {
-          title: "Project 4",
-          description: "This is a really long decsription about this project",
-        },
-      ].map((project) => (
+      {projectsDemo.map((project) => (
         <div>
           {project.title}
           <br />
