58c81e09e5ebf705a2c6c19a238cdb41b12d726c
Nyakamba <omwegaenock@gmail.com>
2023-05-07 10:34:27 +0300

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,7 +30,7 @@ const Projects = () => (
           <BlogCard key={id}>
             <Img src={image} />
             <TitleContent>
-              <HeaderThree title>{title}</HeaderThree>
+              <HeaderThree title="true">{title}</HeaderThree>
               <Hr />
             </TitleContent>
             <CardInfo>{description}</CardInfo>
@@ -42,6 +42,10 @@ const Projects = () => (
                 ))}
               </TagList>
             </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={visit}>Source</ExternalLinks>
+            </UtilityList>
           </BlogCard>
         )
       )}
