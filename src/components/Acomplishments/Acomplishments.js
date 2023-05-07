f1e250a34e528df442bc188f02d46c49140dbe7a
Nyakamba <omwegaenock@gmail.com>
2023-05-07 14:22:03 +0300

    Accomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -10,7 +10,7 @@ import { data } from "../../constants/constants";
 
 const Acomplishments = () => (
   <Section>
-    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <SectionTitle>Acomplishments</SectionTitle>
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
