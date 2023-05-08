b777f26a978c822d1ff7705a78f60ca87447b2b8
Nyakamba <omwegaenock@gmail.com>
2023-05-08 18:52:58 +0300

    Project.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -44,7 +44,7 @@ const Projects = () => (
             </div>
             <UtilityList>
               <ExternalLinks href={visit}>Code</ExternalLinks>
-              <ExternalLinks href={visit}>Source</ExternalLinks>
+              <ExternalLinks href={source}>View site</ExternalLinks>
             </UtilityList>
           </BlogCard>
         )
